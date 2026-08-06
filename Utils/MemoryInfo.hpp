35e0e2a3ec6b4e4ce7c3a3d4c15a25779a2bde05
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-07 02:40:41 +0300

    Update MemoryInfo.hpp

--- Utils/MemoryInfo.hpp
+++ Utils/MemoryInfo.hpp
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <string>
 #include <mach-o/dyld.h>
-#include <mach-o-loader.h> // veya mach-o/loader.h
+#include <mach-o/loader.h> // veya mach-o/loader.h
 
 class MemoryInfo
 {
