ed48dc20ea456a87e80cca52e2b3733fbb802db7
Asasec Media <158528545+asasec@users.noreply.github.com>
2026-08-07 03:25:06 +0300

    Update main.mm

--- main.mm
+++ main.mm
@@ -2,6 +2,7 @@
 #include "IL2CPP_Resolver.hpp"
 #include <fstream>
 #include <thread>
+#include <dlfcn.h>
 
 using namespace IL2CPP;
 
@@ -14,7 +15,7 @@ typedef int (*ImageGetClassCount_t)(void* image);
 typedef void* (*ImageGetClass_t)(void* image, int index);
 typedef void* (*ClassGetMethods_t)(void* klass, void** iter);
 
-// IL2CPP Dahili API İmzalari (İsimler ve metot pointer'ları için)
+// IL2CPP Dahili API İmzalari
 typedef const char* (*il2cpp_class_get_name_t)(void* klass);
 typedef const char* (*il2cpp_class_get_namespace_t)(void* klass);
 typedef const char* (*il2cpp_method_get_name_t)(void* method);
@@ -82,7 +83,6 @@ void ExecuteIl2CppDump() {
     ImageGetClass_t f_ImageGetClass = (ImageGetClass_t)Functions.m_ImageGetClass;
     ClassGetMethods_t f_ClassGetMethods = (ClassGetMethods_t)Functions.m_ClassGetMethods;
 
-    // Dinamik sembol çözme (il2cpp fonksiyonlarını oyun kütüphanesinden buluyoruz)
     il2cpp_class_get_name_t f_ClassName = (il2cpp_class_get_name_t)dlsym(Globals.m_GameFramework, "il2cpp_class_get_name");
     il2cpp_class_get_namespace_t f_ClassNamespace = (il2cpp_class_get_namespace_t)dlsym(Globals.m_GameFramework, "il2cpp_class_get_namespace");
     il2cpp_method_get_name_t f_MethodName = (il2cpp_method_get_name_t)dlsym(Globals.m_GameFramework, "il2cpp_method_get_name");
@@ -132,7 +132,6 @@ void ExecuteIl2CppDump() {
 
                 const char* methodName = f_MethodName ? f_MethodName(method) : "Unknown";
                 
-                // Metot işaretçisini (pointer) güvenli bir şekilde struct üzerinden alıyoruz
                 struct MethodInfo_Internal {
                     void* methodPointer;
                     void* invoker_pointer;
@@ -165,7 +164,7 @@ void ExecuteIl2CppDump() {
 }
 
 @interface ImGuiStyleMenuView : UIView
-@property (nonatomic, strong) UIView *menuWindow;
+@property (nonatomic, strong) UIView *mobileMenuWindow;
 @property (nonatomic, strong) UIButton *floatingIcon;
 @property (nonatomic, strong) UIButton *dumpButton;
 @end
@@ -177,22 +176,24 @@ void ExecuteIl2CppDump() {
     if (self) {
         self.backgroundColor = [UIColor clearColor];
         
-        self.menuWindow = [[UIView alloc] initWithFrame:CGRectMake(50, 80, 270, 110)];
-        self.menuWindow.backgroundColor = [UIColor colorWithRed:0.08 green:0.08 blue:0.10 alpha:0.97];
-        self.menuWindow.layer.cornerRadius = 16.0;
-        self.menuWindow.layer.borderWidth = 1.5;
-        self.menuWindow.layer.borderColor = [UIColor colorWithRed:0.30 green:0.60 blue:1.00 alpha:1.0].CGColor;
-        self.menuWindow.layer.shadowColor = [UIColor blackColor].CGColor;
-        self.menuWindow.layer.shadowOffset = CGSizeMake(0, 8);
-        self.menuWindow.layer.shadowOpacity = 0.5;
-        self.menuWindow.layer.shadowRadius = 10.0;
-        self.menuWindow.clipsToBounds = NO;
-        self.menuWindow.hidden = YES;
-        [self addSubview:self.menuWindow];
+        // --- Ana Mod Menü Penceresi ---
+        self.mobileMenuWindow = [[UIView alloc] initWithFrame:CGRectMake(50, 80, 270, 110)];
+        self.mobileMenuWindow.backgroundColor = [UIColor colorWithRed:0.08 green:0.08 blue:0.10 alpha:0.97];
+        self.mobileMenuWindow.layer.cornerRadius = 16.0;
+        self.mobileMenuWindow.layer.borderWidth = 1.5;
+        self.mobileMenuWindow.layer.borderColor = [UIColor colorWithRed:0.30 green:0.60 blue:1.00 alpha:1.0].CGColor;
+        self.mobileMenuWindow.layer.shadowColor = [UIColor blackColor].CGColor;
+        self.mobileMenuWindow.layer.shadowOffset = CGSizeMake(0, 8);
+        self.mobileMenuWindow.layer.shadowOpacity = 0.5;
+        self.mobileMenuWindow.layer.shadowRadius = 10.0;
+        self.mobileMenuWindow.clipsToBounds = NO;
+        self.mobileMenuWindow.hidden = YES;
+        [self addSubview:self.mobileMenuWindow];
 
         UIPanGestureRecognizer *menuPan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleMenuPan:)];
-        [self.menuWindow addGestureRecognizer:menuPan];
+        [self.mobileMenuWindow addGestureRecognizer:menuPan];
 
+        // --- Başlık Çubuğu ---
         UIView *titleBar = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 270, 40)];
         titleBar.backgroundColor = [UIColor colorWithRed:0.14 green:0.17 blue:0.22 alpha:1.0];
         
@@ -200,7 +201,7 @@ void ExecuteIl2CppDump() {
         CAShapeLayer *maskLayer = [CAShapeLayer layer];
         maskLayer.path = maskPath.CGPath;
         titleBar.layer.mask = maskLayer;
-        [self.menuWindow addSubview:titleBar];
+        [self.mobileMenuWindow addSubview:titleBar];
 
         UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(16, 0, 200, 40)];
         titleLabel.text = @"🐻 IL2CPP Dump Menu";
@@ -208,6 +209,7 @@ void ExecuteIl2CppDump() {
         titleLabel.font = [UIFont boldSystemFontOfSize:13];
         [titleBar addSubview:titleLabel];
 
+        // Kapatma / Küçültme [X] Butonu
         UIButton *closeBtn = [UIButton buttonWithType:UIButtonTypeSystem];
         closeBtn.frame = CGRectMake(230, 8, 24, 24);
         [closeBtn setTitle:@"✕" forState:UIControlStateNormal];
@@ -216,6 +218,7 @@ void ExecuteIl2CppDump() {
         [closeBtn addTarget:self action:@selector(minimizeMenu) forControlEvents:UIControlEventTouchUpInside];
         [titleBar addSubview:closeBtn];
 
+        // --- Tekil Özellik: IL2CPP Dump Başlat Butonu ---
         self.dumpButton = [UIButton buttonWithType:UIButtonTypeSystem];
         self.dumpButton.frame = CGRectMake(18, 56, 234, 38);
         self.dumpButton.backgroundColor = [UIColor colorWithRed:0.20 green:0.60 blue:1.00 alpha:1.0];
@@ -224,8 +227,9 @@ void ExecuteIl2CppDump() {
         self.dumpButton.titleLabel.font = [UIFont boldSystemFontOfSize:14];
         self.dumpButton.layer.cornerRadius = 8.0;
         [self.dumpButton addTarget:self action:@selector(dumpButtonTapped:) forControlEvents:UIControlEventTouchUpInside];
-        [self.menuWindow addSubview:self.dumpButton];
+        [self.mobileMenuWindow addSubview:self.dumpButton];
 
+        // --- Yüzen Ayı Simgesi ---
         self.floatingIcon = [UIButton buttonWithType:UIButtonTypeSystem];
         self.floatingIcon.frame = CGRectMake(40, 100, 54, 54);
         self.floatingIcon.backgroundColor = [UIColor colorWithRed:0.10 green:0.10 blue:0.13 alpha:0.92];
@@ -271,15 +275,15 @@ void ExecuteIl2CppDump() {
 }
 
 - (void)minimizeMenu {
-    self.menuWindow.hidden = YES;
-    self.floatingIcon.center = self.menuWindow.center;
+    self.mobileMenuWindow.hidden = YES;
+    self.floatingIcon.center = self.mobileMenuWindow.center;
     self.floatingIcon.hidden = NO;
 }
 
 - (void)restoreMenu {
     self.floatingIcon.hidden = YES;
-    self.menuWindow.center = self.floatingIcon.center;
-    self.menuWindow.hidden = NO;
+    self.mobileMenuWindow.center = self.floatingIcon.center;
+    self.mobileMenuWindow.hidden = NO;
 }
 
 - (void)dumpButtonTapped:(UIButton *)sender {
@@ -288,3 +292,31 @@ void ExecuteIl2CppDump() {
 }
 
 @end
+
+// Menünün ekranda görünmesini sağlayan otomatik tetikleyici (Constructor)
+__attribute__((constructor)) void initializeDumpMenu() {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIWindow *window = nil;
+        if (@available(iOS 13.0, *)) {
+            for (UIWindowScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                if (scene.activationState == UISceneActivationStateForegroundActive) {
+                    for (UIWindow *win in scene.windows) {
+                        if (win.isKeyWindow) {
+                            window = win;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        if (!window) {
+            window = [UIApplication sharedApplication].windows.firstObject;
+        }
+        
+        if (window) {
+            ImGuiStyleMenuView *menuView = [[ImGuiStyleMenuView alloc] initWithFrame:window.bounds];
+            menuView.userInteractionEnabled = YES;
+            [window addSubview:menuView];
+        }
+    });
+}
